8dd7faf7c5a150fb987720cbe0ad702e7241e538
michelle <kwong.meshell@gmail.com>
2020-08-21 23:11:20 -0700

    cors setup & frontend setup

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,49 @@
 console.log('hey')
 
-console.log('afhiahfa')
+const content = document.getElementById('content');
+const nav = document.getElementById('nav');
+
+nav.addEventListener('click', (e) => {
+    if(e.target.id == 'bulletin'){
+        console.log('clicked');
+        getBulletin();
+    }
+    else if(e.target.id == 'book-bag'){
+        getBookBag();
+    }
+})
+
+//bulletin board
+async function getBulletin() {
+    console.log('getting bulletin')
+    const response = await fetch('https://enigmatic-waters-10084.herokuapp.com/bulletin', {
+        method: 'GET',
+        mode: 'cors'
+    })
+
+    const bulletins = await response.json();
+
+    bulletins.bulletins.forEach((bulletin) => {
+
+        //do dom stuff here
+        console.log(bulletin);
+    });
+}
+
+//bookbag
+async function getBookBag() {
+    console.log('getting bookbag')
+    const response = await fetch('https://enigmatic-waters-10084.herokuapp.com/bookBag', {
+        method: 'GET',
+        mode: 'cors'
+    })
+
+    const summaries = await response.json();
+
+    summaries.summaries.forEach((summary) => {
+
+        //do dom stuff here
+        console.log(summary);
+    });
+}
+
